6cbcb8f0c9c6dfaf37098c8e33b856bc7d403925
z-z-12 <19283623981@163.com>
2026-01-10 15:54:29 +0800

    Site updated: 2026-01-10 15:54:28

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["2026/01/07/hexo部署/","2026/01/08/s/","2026/01/07/安知鱼主题安装/","2026/01/08/字数统计/","2026/01/08/搜索栏/","2026/01/08/永久链接/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
